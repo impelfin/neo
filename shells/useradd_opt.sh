91522ae27e4833fdb6e79f8098aa25effa9fc9cf
impelfin <impelfin@gmail.com>
2025-03-17 10:19:16 +0900

    useradd_opt shell script

--- shells/useradd_opt.sh
+++ shells/useradd_opt.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 #
-set -- $(getopt -q ab:c:de "$@")
+set -- $(getopt -q u:g:c:d:s:k:m "$@")
 #
 echo 
 while [ -n "$1" ]
 do
     case "$1" in
-        -a) echo "Found the -a option";;
-        -b) param="$2"
-            echo "Found -b option, with paremeter value $param"
+        -u) param="$2"
+            echo "-u (uid) option, with paremeter value $param"
+            shift;;
+        -g) param="$2"
+            echo "-g (gid) option, with paremeter value $param"
             shift;;
         -c) param="$2"
-            echo "Found -c option, with paremeter value $param"
+            echo "-c (comment) option, with paremeter value $param"
             shift;;
+        -d) param="$2"
+            echo "-d (home directory) option, with paremeter value $param"
+            shift;;
+        -s) param="$2"
+            echo "-s (shell) option, with paremeter value $param"
+            shift;;
+        -k) param="$2"
+            echo "-k (inital scripts directory) option, with paremeter value $param"
+            shift;;
+        -m) echo "-m (make home directory) option";;
         --) shift
             break;;
         *) echo "$1 is not an option" ;;
