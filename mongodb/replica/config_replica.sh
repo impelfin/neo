231493436f1926d77595b5cde3ac673bde121fbb
impelfin <impelfin@gmail.com>
2025-03-27 18:01:13 +0900

    config replica

--- mongodb/replica/config_replica.sh
+++ mongodb/replica/config_replica.sh
@@ -20,12 +20,7 @@ else
 fi
 
 # MongodB Daemon Start
-/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/master.conf &
-/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/slave1.conf &
-/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/slave2.conf &
-/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/arbiter.conf &
-netstat -ntlp | grep mongo
-sleep 3s
+./start_replica.sh
 
 # Replica Init Setup
 /replica/mongodb/bin/mongo --port 10000 < rs_start    
