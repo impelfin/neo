9e27aef72829a85ad2f3415d535027064e0f33d3
impelfin <impelfin@gmail.com>
2025-03-28 15:20:13 +0900

    sync mysql nodejs

--- node/mysql/app.js
+++ node/mysql/app.js
@@ -6,30 +6,31 @@ var app = express();
 var connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
+    port: process.env.port,
     password: process.env.password,
     database: process.env.database
 });
 
 connection.connect(function (err) {
     if (!err) {
-        console.error('Database is connected~!!\n\n');
+        console.log("Database is connected~!!\n\n");
     } else {
-        console.log('Error connecting Database~!!\n\n');
+        console.log("Error connecting Database~!!")
     }
-});
+})
 
 app.get('/', function (req, res) {
-    connection.query('SELECT * FROM st_info', function (err, rows, fields) {
+    connection.query('select * from st_info', function (err, rows, fields) {
         connection.end();
-        if (err) {
+        if (!err) {
             res.send(rows);
             console.log("The solution is : ", rows);
         } else {
-            console.log('Error while performing Query~!!\n\n');
+            console.log('Error while performing Query~!!\n');
         }
-    })   
+    })
 })
 
 app.listen(8000, function () {
-    console.log("8000 Port : Server Started~!!\n\n");
+    console.log('8000 Port : Server Started~!!\n');
 })
