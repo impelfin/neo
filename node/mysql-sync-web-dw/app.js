d4e9aec71eff673d1318a88c03d16ec64f56abda
impelfin <impelfin@gmail.com>
2025-03-31 13:25:15 +0900

    update

--- node/mysql-sync-web-dw/app.js
+++ node/mysql-sync-web-dw/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.set('port', process.env.Port || 8000);
-app.use(morgan('dev'));
+app.set("port", process.env.Port || 8000);
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-var main = require('./routes/main');
-app.use('/', main);
+var main = require("./routes/main");
+app.use("/", main);
 
-app.listen(app.get('port'), function () {
-    console.log("Server is Started~!!" + app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("Server is Started~!! Port : " + app.get("port"));
 });
