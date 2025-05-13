5999d16eb783d7d065f9c123f661764a3f686ee9
JunHyen Mun <impelfin@gmail.com>
2025-05-13 17:55:11 +0900

    k8s_shutdown.sh

---
+++ k8s/shutdown1/k8s_shutdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+node_user="root"
+node_ip="192.168.1.211"
+
+echo "Shutting down K8S Node ($node_ip)..."
+
+ssh ${node_user}@${node_ip} "nohup shutdown -h now > /dev/null 2>&1 &"
+
+if [ $? -eq 0 ]; then
+    echo "K8S Node shut down command sent successfully."
+    echo "Shutting down K8S Master..."
+    sudo shutdown now
+else
+    echo "Failed to send shutdown command to K8S Node. Aborting shutdown of K8S Master."
+fi
