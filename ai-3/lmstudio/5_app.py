b62af3407011452dea936c79f1abf6158c2ee12c
JunHyen Mun <impelfin@gmail.com>
2025-06-10 10:06:31 +0900

    Update 5_app.py

--- ai-3/lmstudio/5_app.py
+++ ai-3/lmstudio/5_app.py
@@ -6,7 +6,7 @@ image_handle = lms.prepare_image(image_path)
 model = lms.llm('google/gemma-3-4b')
 chat = lms.Chat()
 
-chat.add_user_message("Describe this image please.", image=image_handle)
+chat.add_user_message("Describe this image please.", images=[image_handle])
 
 prediction = model.respond(chat)
 
