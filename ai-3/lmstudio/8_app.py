aff7682c4543d1e5b338d821d06a508fca950b01
JunHyen Mun <impelfin@gmail.com>
2025-06-10 10:28:06 +0900

    Update 8_app.py

--- ai-3/lmstudio/8_app.py
+++ ai-3/lmstudio/8_app.py
@@ -9,18 +9,15 @@ while True:
     except EOFError:
         print()
         break
-
-    if user_command.strip == "exit":
+    if user_command.strip() == "exit":
         break
-
-    console_history.append(f'$ {user_command}')
-    history_prompt = '\n'.join(console_history)
-    prediction_stream = model.respond_stream(
-        history_prompt, 
-        config = { 'stopStrings': ["$"] },
+    console_history.append(f"$ {user_command}")
+    history_prompt = "\n".join(console_history)
+    prediction_stream = model.complete_stream(
+        history_prompt,
+        config={ "stopStrings": ["$"] },
     )
-
     for fragment in prediction_stream:
-        print(fragment.content, end='', flush=True)
+        print(fragment.content, end="", flush=True)
     print()
     console_history.append(prediction_stream.result().content)
